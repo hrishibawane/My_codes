d05ec91cd4f44c490d68c22178bcd86e71668d87
Hrishikesh Bawane <hrishibawane.hvb@gmail.com>
2017-07-04 10:03:45 -0400

    Update sets.cpp

--- sets.cpp
+++ sets.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 void getSet(int set[], int size)
 {
@@ -6,7 +7,6 @@ void getSet(int set[], int size)
 	for(i=0; i<size; i++)
 		scanf("%d", &set[i]);
 }
-	
 void printSet(int set[], int size)
 {
 	int i;
@@ -18,54 +18,74 @@ void sort(int set[], int size)
 {
 	int i, j;
 	for(i=0; i<size-1; i++)
-	{
-		int flag = 0;
 		for(j=i+1; j<size; j++)
-		{
 			if(set[i] > set[j])
 			{
 				int temp = set[i];
 				set[i] = set[j];
 				set[j] = temp;
-				flag = 1;		
-			}		
-		}
-		if(!flag) break;
-	}
+			}
 }
 void setUnion(int setA[], int setB[], int n, int m)
 {
-	int setC[n+m];
-	for(int i=0; i<n; i++)
+	int setC[n+m], i, j;
+	for(i=0; i<n; i++)
 		setC[i] = setA[i];
-	for(int i=0, j=n; i<m; i++, j++)
+	for(i=0, j=n; i<m; i++, j++)
 		setC[j] = setB[i];
 
 	sort(setC, n+m);
-	for(int i=0; i<n+m; i++)
+	for(i=0; i<n+m; i++)
 	{
 		printf("%d ", setC[i]);
-		if(setC[i]==setC[i+1]) i++;	
+		if(setC[i]==setC[i+1]) i++;
 	}
-
+    printf("\n");
 }
 void intersect(int setA[], int setB[], int n, int m)
 {
-	int setC[n+m];
-	for(int i=0; i<n; i++)
+	int setC[n+m], i, j;
+	for(i=0; i<n; i++)
 		setC[i] = setA[i];
-	for(int i=0, j=n; i<m; i++, j++)
+	for(i=0, j=n; i<m; i++, j++)
 		setC[j] = setB[i];
 
 	sort(setC, n+m);
-	for(int i=0; i<n+m-1; i++)
-	{
+	for(i=0; i<n+m-1; i++)
 		if(setC[i]==setC[i+1])
 		{
 			printf("%d ", setC[i]);
 			i++;
 		}
-	}
+	
+	printf("\n");
+}
+void difference(int setA[], int setB[], int n, int m)
+{
+    int i, j;
+    for(i=0; i<n; i++)
+        for(j=0; j<m; j++)
+            if(setA[i]==setB[j])
+                setA[i] = -1;
+
+    for(i=0; i<n; i++)
+        if(setA[i] != -1) printf("%d ", setA[i]);
+
+    printf("\n");
+}
+void sym_diff(int setA[], int setB[], int n, int m)
+{
+    int i, j;
+    for(i=0; i<n; i++)
+        for(j=0; j<m; j++)
+            if(setA[i]==setB[j])
+                setA[i] = setB[j] = -1;
+
+    for(i=0; i<n; i++)
+        if(setA[i] != -1) printf("%d ", setA[i]);
+    for(i=0; i<m; i++)
+        if(setB[i] != -1) printf("%d ", setB[i]);
+    printf("\n");
 }
 int main()
 {
@@ -75,13 +95,29 @@ int main()
 	int setA[n];
 	printf("Enter size of set B\n");
 	scanf("%d", &m);
-	int setB[n];
+	int setB[m];
 	printf("Enter elements of set A without repetition\n");
 	getSet(setA, n);
 	printf("Enter elements of set B without repetition\n");
 	getSet(setB, m);
-	printf("\nUnion : ");
-	setUnion(setA, setB, n, m);
-	printf("\nIntersection : ");
-	intersect(setA, setB, n, m);
+	int choice;
+	printf("Enter your choice:\n1. Union\n2. Intersection\n3. Difference\n4. Symmetric difference\n");
+	scanf("%d", &choice);
+	switch(choice)
+	{
+	    case 1: printf("Union: ");
+	        setUnion(setA, setB, n, m);
+	        break;
+        case 2: printf("Intersection: ");
+            intersect(setA, setB, n, m);
+            break;
+        case 3: printf("Difference: ");
+            difference(setA, setB, n, m);
+            break;
+        case 4: printf("Symmetric Difference: ");
+            sym_diff(setA, setB, n, m);
+            break;
+        default: printf("Wrong choice\n");
+	}
+	return 0;
 }
