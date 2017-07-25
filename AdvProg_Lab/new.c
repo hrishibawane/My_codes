b558f050d450cabcc090353350c03fcfb0ff7227
Hrishikesh Bawane <hrishibawane.hvb@gmail.com>
2017-07-25 09:42:41 +0530

    Add files via upload

---
+++ AdvProg_Lab/new.c
@@ -0,0 +1,105 @@
+//represent matrix using 2d arrays &perform foll ops:
+//addition, mult, transpose, saddle point
+
+#include <stdio.h>
+
+void accept(int m[50][50], int row, int col)
+{
+	for(int i=0; i<row; i++)
+		for(int j=0; j<col; j++)
+			scanf("%d", &m[i][j]);
+}
+
+void display(int m[50][50], int row, int col)
+{
+	for(int i=0; i<row; i++)
+	{
+		for(int j=0; j<col; j++)
+			printf("%d ", m[i][j]);
+		printf("\n");
+	}
+}
+
+void add(int a[50][50], int b[50][50], int row, int col)
+{
+	int c[50][50];
+	for(int i=0; i<row; i++)
+		for(int j=0; j<col; j++)
+			c[i][j] = a[i][j] + b[i][j];
+
+	display(c, row, col);
+}
+
+void multiply(int a[50][50], int b[50][50], int row1, int col1, int row2, int col2)
+{
+	int c[50][50];
+	for(int i=0; i<row1; i++)
+		for(int j=0; j<col2; j++)
+			for(int k=0; k<col1; k++)
+				c[i][j] += a[i][k]*b[k][j];
+
+	display(c, row1, col2);
+}
+
+void transpose(int a[50][50], int row, int col)
+{
+
+	for(int i=0; i<col; i++)
+	{
+		for(int j=0; j<row; j++)
+			printf("%d ", a[j][i]);
+
+		printf("\n");
+	}
+}
+
+int main()
+{
+int ch;
+int a[50][50], b[50][50], row1, row2, col1, col2;
+while(1)
+{
+	printf("Enter your choice:\n1. Accept\n2. Display\n3. Add\n4. Multiply\n5. Transpose\n6. Saddle point\n");
+	scanf("%d", &ch);
+	switch(ch)
+	{
+		case 1: printf("\nEnter row & col size of matrix A: ");
+				scanf("%d %d", &row1, &col1);
+				printf("\nEnter elements of matrix A:");
+				accept(a, row1, col1);
+				printf("\nEnter row & col size of matrix B: ");
+				scanf("%d %d", &row2, &col2);
+				printf("\nEnter elements of matrix B:");
+				accept(b, row2, col2);
+				break;
+
+		case 2: printf("\nElements of A\n"); display(a, row1, col1);
+				printf("\nElements of B\n"); display(b, row2, col2);
+				break;
+
+		case 3: if(row1==row2 && col1==col2)
+				{
+					printf("\nMatrix A + Matrix B =\n");
+					add(a, b, row1, col1);
+				}
+				else printf("\nAddition not possible");
+				break;
+
+		case 4: if(col1==row2)
+				{
+					printf("\nMatrix A x Matrix B = \n");
+					multiply(a, b, row1, col1, row2, col2);
+				}
+				else printf("\nMultiplication not possible");
+				break;
+		
+		case 5: printf("\nTranspose of A:\n"); transpose(a, row1, col1);
+				printf("\nTranspose of B:\n"); transpose(b, row2, col2);
+				break;
+
+		case 6: printf("Saddle Point: ");
+				
+		
+	}
+}
+}
